41ffd9ab78fa0a9a27ea44d5d2d7a6534c0d84a5
Daniel De La Pena <del17011@byui.edu>
2025-01-12 03:25:47 -0700

    recipehub skeleton setup first part

---
+++ src/router.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./app"
+import Home from "./pages/Home"
+
+const router = () => {
+    return(
+        <Router>
+            <Routes>
+                <Route path="/" element={<App />}/>
+                <Route path="/home" element={<Home />}/>
+            </Routes>
+        </Router>
+    )
+}
+
+export default router;
