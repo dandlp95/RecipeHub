374debd79dad70d7ae3c34d9f25453e3e42431c9
Daniel De La Pena <del17011@byui.edu>
2025-02-18 17:08:55 -0700

    stopping point

--- src/components/groupsContainer.tsx
+++ src/components/groupsContainer.tsx
@@ -9,26 +9,64 @@ import { PathParams } from '../customTypes/requestTypes'
 import { ErrorHandling } from '../customTypes/errorHandling'
 import { ImBin2 } from 'react-icons/im'
 
+// This component is responsible for displaying the groups that the user has created.
+
+/******************INDIVIDUAL GROUP ********************/
+type Props1 = {
+  group: Group
+  passGroupName: (groupName: string | null) => void
+  handleGroupDeletion: (groupId: number) => void
+}
+
+const IndividualGroup: React.FunctionComponent<Props1> = ({
+  group,
+  passGroupName,
+  handleGroupDeletion
+}) => {
+  return (
+    <div key={group.groupId}>
+      <div className={groupContainerCSS.groupName}>
+        <IconContext.Provider
+          value={{ className: `${groupContainerCSS.deleteIcon}` }}
+        >
+          <ImBin2
+            onClick={() =>
+              group.groupId ? handleGroupDeletion(group.groupId) : ''
+            }
+          />
+        </IconContext.Provider>
+        <span onClick={() => passGroupName(group.name)}>{group.name}</span>
+      </div>
+      <p>{`${group.totalRecipes} recipes`}</p>
+    </div>
+  )
+}
+
+/**************MAING GROUP CONTAINER FUNCTION COMPONENT************/
 type Props = {
   passGroupId: (groupId: number | null) => void
   passGroupName: (groupName: string | null) => void
-}
-
-const getGroups = async (
-  userId: number,
-  setState: (groups: Group[]) => void
-) => {
-  const apiService = createRecipeApiService()
-  const fetchedGroups = await apiService.getGroups(`users/${userId}/groups`, {
-    userId: userId
-  })
-  setState(fetchedGroups?.result ? fetchedGroups.result : [])
 }
 
 const GroupContainer: React.FunctionComponent<Props> = (props: Props) => {
   const [groups, setGroups] = useState<Group[]>([])
   const [isEditing, setIsEditing] = useState<boolean>(false)
   const newGroupRef = useRef<HTMLInputElement | null>(null)
+
+  /*******************************************************
+   *            HANDLE FETCHING GROUPS
+   * *************************************************** */
+
+  const getGroups = async (
+    userId: number,
+    setState: (groups: Group[]) => void
+  ) => {
+    const apiService = createRecipeApiService()
+    const fetchedGroups = await apiService.getGroups(`users/${userId}/groups`, {
+      userId: userId
+    })
+    setState(fetchedGroups?.result ? fetchedGroups.result : [])
+  }
 
   useEffect(() => {
     try {
@@ -50,6 +88,10 @@ const GroupContainer: React.FunctionComponent<Props> = (props: Props) => {
       console.error(errorHandling)
     }
   }, [])
+
+  /*******************************************************
+   *            HANDLE GROUP CREATION
+   * *************************************************** */
 
   const addGroup = async () => {
     const userId = Number(localStorage.getItem('userId'))
