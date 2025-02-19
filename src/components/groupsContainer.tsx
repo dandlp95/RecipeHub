ab31d7122f9942f9667234c953905c4c089c0f2f
Daniel De La Pena <del17011@byui.edu>
2025-02-19 01:11:33 -0700

    implement editable group names with dynamic updates and styling

--- src/components/groupsContainer.tsx
+++ src/components/groupsContainer.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState, useRef, useCallback } from 'react'
 import groupContainerCSS from './styles/groupsContainer.module.css'
 import { AiOutlinePlus } from 'react-icons/ai'
 import { IconContext } from 'react-icons'
@@ -12,19 +12,51 @@ import { ImBin2 } from 'react-icons/im'
 // This component is responsible for displaying the groups that the user has created.
 
 /******************INDIVIDUAL GROUP ********************/
+
 type Props1 = {
   group: Group
-  passGroupName: (groupName: string | null) => void
   handleGroupDeletion: (groupId: number) => void
+  onUpdate: (groupName: Group) => void
+  passGroupName: (groupName: string | null) => void
 }
 
 const IndividualGroup: React.FunctionComponent<Props1> = ({
   group,
-  passGroupName,
-  handleGroupDeletion
+  handleGroupDeletion,
+  onUpdate,
+  passGroupName
 }) => {
+  const [isEditing, setIsEditing] = useState<boolean>(false)
+  const [groupData, setGroupData] = useState<Group>(group)
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
+  const saveUpdate = useCallback((): void => {
+    // When losing focus, the group name is updated (if changed)
+    onUpdate(groupData)
+  }, [groupData, onUpdate])
+
+  const finishEditHandler = () => {
+    // Ends editing and saves the updated group name (if changed)
+    setIsEditing(false)
+    saveUpdate()
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    // Ends editing and saves the updated group name (if changed) when 'Enter' is pressed
+    if (e.key === 'Enter') {
+      finishEditHandler()
+    }
+  }
+
+  useEffect(() => {
+    if (isEditing && inputRef.current) {
+      inputRef.current.focus()
+      inputRef.current.select()
+    }
+  }, [isEditing])
+
   return (
-    <div key={group.groupId}>
+    <div key={group.groupId} className={groupContainerCSS.singleGroup}>
       <div className={groupContainerCSS.groupName}>
         <IconContext.Provider
           value={{ className: `${groupContainerCSS.deleteIcon}` }}
@@ -35,7 +67,23 @@ const IndividualGroup: React.FunctionComponent<Props1> = ({
             }
           />
         </IconContext.Provider>
-        <span onClick={() => passGroupName(group.name)}>{group.name}</span>
+        {isEditing ? (
+          <input
+            onChange={e => setGroupData({ ...groupData, name: e.target.value })}
+            value={groupData.name ? groupData.name : ''}
+            onBlur={() => finishEditHandler()}
+            ref={inputRef}
+            onKeyDown={e => handleKeyDown(e)}
+            type='text'
+          />
+        ) : (
+          <span
+            onClick={() => passGroupName(groupData.name)}
+            onDoubleClick={() => setIsEditing(true)}
+          >
+            {group.name}
+          </span>
+        )}
       </div>
       <p>{`${group.totalRecipes} recipes`}</p>
     </div>
@@ -173,6 +221,57 @@ const GroupContainer: React.FunctionComponent<Props> = (props: Props) => {
     }
   }
 
+  /****************************************************************
+   *                HANDLE GROUP UPDATES
+   * ************************************************************** */
+
+  const updateGroup = async (groupId: number, updatedGroup: Group) => {
+    const userId = Number(localStorage.getItem('userId'))
+    const token = localStorage.getItem('token')
+
+    if (isNaN(userId)) {
+      throw new Error('User ID is not a number')
+    }
+    if (!token) {
+      throw new Error('Token not found')
+    }
+
+    const url = `users/${userId}/groups/${groupId}`
+
+    const apiService = createRecipeApiService()
+    const response = await apiService.updateGroup(
+      url,
+      {
+        userId: userId,
+        groupId: groupId
+      },
+      updatedGroup
+    )
+
+    return response.result
+  }
+
+  const onUpdate = async (groupName: Group) => {
+    // Updates entity group and passes updated group name to parent component.
+    try {
+      const updatedGroup = await updateGroup(
+        groupName.groupId ? groupName.groupId : 0,
+        groupName
+      )
+      if (updatedGroup?.groupId) {
+        setGroups(prevgroups =>
+          prevgroups.map(group =>
+            group.groupId === groupName.groupId ? updatedGroup : group
+          )
+        )
+        props.passGroupId(updatedGroup.groupId)
+        props.passGroupName(updatedGroup.name)
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <section className={groupContainerCSS.groupContainerMain}>
       <div className={groupContainerCSS.top}>
@@ -188,23 +287,13 @@ const GroupContainer: React.FunctionComponent<Props> = (props: Props) => {
       <div className={groupContainerCSS.groups}>
         {groups &&
           groups.map(group => (
-            <div key={group.groupId}>
-              <div className={groupContainerCSS.groupName}>
-                <IconContext.Provider
-                  value={{ className: `${groupContainerCSS.deleteIcon}` }}
-                >
-                  <ImBin2
-                    onClick={() =>
-                      group.groupId ? handleGroupDeletion(group.groupId) : ''
-                    }
-                  />
-                </IconContext.Provider>
-                <span onClick={() => props.passGroupName(group.name)}>
-                  {group.name}
-                </span>
-              </div>
-              <p>{`${group.totalRecipes} recipes`}</p>
-            </div>
+            <IndividualGroup
+              key={group.groupId}
+              group={group}
+              handleGroupDeletion={handleGroupDeletion}
+              onUpdate={onUpdate}
+              passGroupName={props.passGroupName}
+            />
           ))}
       </div>
     </section>
